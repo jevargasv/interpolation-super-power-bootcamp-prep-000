0a2fb26c278ad9c7db2e04f7a3895886b05e80dc
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-25 00:34:17 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(color_array)
-  puts "R: #{color_array[0]}, O: #{color_array[1]}, Y: #{color_array[2]}, G: #{color_array[3]}, B: , I: indigo, V: violet"
+  puts "R: #{color_array[0]}, O: #{color_array[1]}, Y: #{color_array[2]}, G: #{color_array[3]}, B: #{color_array[4]}, I: #{color_array[5]}, V: #{color_array[6]}"
 end
